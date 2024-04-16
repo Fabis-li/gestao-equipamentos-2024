37bc77908cd12f3bd1df98289759568d827282c2
Fabiano Silva Lima <fabiano_slima@hotmail.com>
2024-04-16 17:36:48 -0300

    acao cadastrar

---
+++ src/GestaoEquipamentos.ConsoleApp/CadastrarEquipamento.cs
@@ -0,0 +1,30 @@
+﻿namespace GestaoEquipamentos.ConsoleApp
+{
+    public class CadastrarEquipamento
+    {       
+        public RepositorioEquipamentos repositorio = new RepositorioEquipamentos();        
+        public void ObterDados()
+        {
+            Equipamento equipamento = new Equipamento();
+
+            Console.WriteLine("Cadastro de Equipamentos\n");
+            Console.WriteLine("Registrando um novo Equipamento\n");
+            Console.Write("Digite o nome do equipamento: ");
+            equipamento.Nome = Console.ReadLine();            
+            Console.Write("\nDigite o preço do equipamento: ");
+            equipamento.PrecoAquisicao = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("\nDigite o número de serie do equipamento: ");
+            equipamento.NumeroSerie = Console.ReadLine();
+            Console.Write("\nDigite a data de fabricação: ");
+            equipamento.DataFabricacao = Console.ReadLine();
+            Console.Write("\nDigite o fabricante: ");
+            equipamento.Fabricante = Console.ReadLine();
+
+            repositorio.RegistrarEquipamento(equipamento);
+        }
+
+        
+
+        
+    }
+}
---
+++ src/GestaoEquipamentos.ConsoleApp/RepositorioEquipamentos.cs
@@ -0,0 +1,22 @@
+﻿namespace GestaoEquipamentos.ConsoleApp
+{    
+    public class RepositorioEquipamentos
+    {      
+        private Equipamento[] equipamentos = new Equipamento[100];
+        public int contadorEquipamentos;
+
+        public void RegistrarEquipamento(Equipamento equipamento)
+        {
+            equipamentos[contadorEquipamentos]  = equipamento;
+            contadorEquipamentos++;
+        }
+
+        public void ListarEquipamentos()
+        {
+            for (int i = 0;  i < equipamentos.Length; i++)
+            {
+                Console.WriteLine(equipamentos[i].ToString());
+            }
+        }
+    }
+}
